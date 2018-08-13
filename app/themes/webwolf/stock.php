041662833c1a7ec5ce59bc80d6309e4f9d264ab5
Vadim Mishkhozhev <mishogev@yandex.ru>
2018-08-13 19:55:03 +0700

    add template in stock

--- app/themes/webwolf/stock.php
+++ app/themes/webwolf/stock.php
@@ -1,14 +1,20 @@
 <?php
 /**
- * The template for displaying all single posts
+ * The template for displaying all pages
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package WebWolf24
  * Template Name: Акции
  */
 
 get_header();
+get_sidebar();
 ?>
 
 	<div id="primary" class="content-area">
@@ -19,8 +25,6 @@ get_header();
 			the_post();
 
 			get_template_part( 'template-parts/content', 'stock' );
-
-			the_post_navigation();
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -34,5 +38,4 @@ get_header();
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();

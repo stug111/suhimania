4ebf127e920709e690c8f82c4b0a6b5f4b11828f
Vadim Mishkhozhev <mishogev@yandex.ru>
2018-08-13 19:50:06 +0700

    add template in stock

--- app/themes/webwolf/template-parts/content-stock.php
+++ app/themes/webwolf/template-parts/content-stock.php
@@ -16,33 +16,6 @@
 
     <div class="entry-content">
         <?php the_content(); ?>
-        <?php $stocks = CFS()->get( 'stock_block' );
-        if( ! empty($stocks) ): ?>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-12">
-                        <?php foreach ( $stocks as $key => $stock ) { ?>
-                            <?php
-
-                            ($key%2) ? $class = 'before-list' : $class = 'after-list';
-
-                            ?>
-                            <div class="stock__block" style="background: url('<?= $stock['stock_image'] ?>') no-repeat center center / cover">
-                                <h2 class="stock__title <?= $class ?>">
-
-                                </h2>
-                                <div class="stock__sub-title <?= $class ?>">
-                                    <?= $stock['stock_description'] ?> <span>*</span>
-                                </div>
-                                <div class="stock__bottom">
-                                    <span>*</span> <?= $stock['stock_advanced'] ?>
-                                </div>
-                            </div>
-                        <?php } ?>
-                    </div>
-                </div>
-            </div>
-        <?php endif; ?>
     </div><!-- .entry-content -->
 
 </article><!-- #post-<?php the_ID(); ?> -->
